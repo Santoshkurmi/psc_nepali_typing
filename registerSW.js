e59f49efa3af1d01810dc378b9589e98cd028da4
Santosh kurmi <xantosh121@gmail.com>
2025-08-16 10:10:04 +0545

    initital nepali typing

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/psc_nepali_typing/sw.js', { scope: '/psc_nepali_typing/' })})}
